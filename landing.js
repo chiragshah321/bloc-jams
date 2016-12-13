394f30ab3c0563ea0e9520ce4046798f24d6ce8d
Chirag Shah <chiragshah321@gmail.com>
2016-12-13 16:06:25 -0500

    assignment checkpoint completed after video

--- landing.js
+++ landing.js
@@ -1,12 +1,15 @@
-var animatePoints = document.getElementsByClassName('point');
+var animatePoints = function(){
 
-function revealPoint() {
+var points = document.getElementsByClassName('point');
 
-    for(var i = 0; i <= animatePoints.length; i++ ) {
+var revealPoint = function(i){
     points[i].style.opacity = 1;
     points[i].style.transform = "scaleX(1) translateY(0)";
     points[i].style.msTransform = "scaleX(1) translateY(0)";
     points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-
   };
+
+    for (var i = 0; i < points.length; i++ ) {
+      revealPoint(i)
+    }
 };
