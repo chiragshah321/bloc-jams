2c51f714c52388040dabd6d3a6a72d91b3542d77
Chirag Shah <chiragshah321@gmail.com>
2016-12-14 10:25:35 -0500

    after video submission

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,16 +1,6 @@
-var pointsArray = document.getElementsByClassName('point');
+function forEach(array, callback){
+  for(var i = 0; i<array.length; i++){
+    callback(array[i]); 
 
-function forEach (){
-  for(var i = 0; i < pointsArray.length; i++){
-
-  points[index].style.opacity = 1;
-  points[index].style.transform = "scaleX(1) translateY(0)";
-  points[index].style.msTransform = "scaleX(1) translateY(0)";
-  points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-
-};
-}
-
-pointsArray.forEach(){
-
+  }
 }
