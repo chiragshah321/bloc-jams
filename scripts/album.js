6f267dc919275ef6d2c92ea5ce96d580258ccb1f
Chirag Shah <chiragshah321@gmail.com>
2016-12-20 09:36:47 -0500

    checkpoint 11 changes to master file

--- scripts/album.js
+++ scripts/album.js
@@ -30,17 +30,17 @@ var albumMarconi = {
 
 };
 
-var createSongRow = function(songNumber, songName, songLength){
+var createSongRow = function(songNumber, songName, songLength) {
     var template =
-      '<tr class = "album-view-song-item">'
-      + '<td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
-      + '<td class = "song-item-title">' + songName +  '</td>'
-      + '<td class = "song-item-duration">' + songLength +  '</td>'
-      + '</tr>'
+       '<tr class="album-view-song-item">'
+     + '  <td class="song-item-number">' + songNumber + '</td>'
+     + '  <td class="song-item-title">' + songName + '</td>'
+     + '  <td class="song-item-duration">' + songLength + '</td>'
+     + '</tr>'
+     ;
 
     return template;
-
-}
+};
 
 var setCurrentAlbum = function(album) {
 
