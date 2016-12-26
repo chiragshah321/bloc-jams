ed33ec75c11c3bbb04908143752528056e842a03
Chirag Shah <chiragshah321@gmail.com>
2016-12-26 15:55:28 -0500

    after video assignment update

--- scripts/album.js
+++ scripts/album.js
@@ -1,3 +1,12 @@
+var setSong =  function(songNumber) {
+  currentlyPlayingSongNumber = parseInt(songNumber);
+  currentSongFromAlbum = currentAlbum.songs[songNumber -1];
+};
+
+var getSongNumberCell = function(number) {
+  return $('.song-item-number[data-song-number="' + number + '"]');
+}
+
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
        '<tr class="album-view-song-item">'
@@ -14,15 +23,14 @@ var createSongRow = function(songNumber, songName, songLength) {
       var songNumber = parseInt($(this).attr('data-song-number'));
 
 	    if (currentlyPlayingSongNumber !== null) {
-		    var currentlyPlayingCell = parseInt($('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]'));
+		    var currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
 		    currentlyPlayingCell.html(currentlyPlayingSongNumber);
 
       }
 
 	   if (currentlyPlayingSongNumber !== songNumber) {
 		    $(this).html(pauseButtonTemplate);
-		    currentlyPlayingSongNumber = songNumber;
-        currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+		    setSong(songNumber);
         updatePlayerBarSong();
       }
 
